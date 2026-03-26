ed38117b5133e69217ea79f057b3237d967b3c63
umayabara3 <umayabara3@gmail.com>
2026-03-26 11:18:47 +0900

    タイムの考慮

--- hello.go
+++ hello.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", "https://httpbin.org/delay/3", nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		if ctx.Err() == context.DeadlineExceeded {
+			fmt.Println("Request timed out")
+		} else {
+			fmt.Println("Error:", err)
+		}
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Request succeeded")
 }
